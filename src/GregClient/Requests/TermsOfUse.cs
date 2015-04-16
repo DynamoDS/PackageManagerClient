e0ea64990fab18c7baf0ba6d95f71593c208dce0
Benglin (Ben) Goh <benglin@outlook.com>
2015-04-16 18:09:09 +0800

    Added terms of use related requests/responses

---
+++ src/GregClient/Requests/TermsOfUse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+
+namespace Greg.Requests
+{
+    public class TermsOfUse : Request
+    {
+        private readonly Method httpMethod = Method.GET;
+
+        public TermsOfUse(bool queryAcceptanceStatus)
+        {
+            httpMethod = queryAcceptanceStatus ? Method.GET : Method.PUT;
+        }
+
+        public override string Path
+        {
+            get { return "tou"; }
+        }
+
+        public override Method HttpMethod
+        {
+            get { return httpMethod; }
+        }
+
+        internal override void Build(ref RestRequest request)
+        {
+        }
+    }
+}
