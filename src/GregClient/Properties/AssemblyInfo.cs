714af3eed559ae84d9511dda5787ac5d61ece7e0
Ian Keough <ian.keough@autodesk.com>
2016-02-16 11:38:34 -0800

    Make Greg NuGet package.

--- src/GregClient/Properties/AssemblyInfo.cs
+++ src/GregClient/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("greg-client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Greg")]
+[assembly: AssemblyDescription("The Dynamo Package Manager .NET client.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Autodesk, Inc.")]
-[assembly: AssemblyProduct("greg-client")]
-[assembly: AssemblyCopyright("Copyright © Autodesk, Inc. 2015")]
+[assembly: AssemblyProduct("Greg")]
+[assembly: AssemblyCopyright("Copyright © Autodesk, Inc. 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
