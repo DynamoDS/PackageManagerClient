e29533ffc3b5e89b4bf760bdc1ad0a488b9e8b9f
Ian Keough <ian.keough@autodesk.com>
2016-04-21 10:08:00 -0700

    Revert "Revert "Consolidate nuget stuff. Add install script.""
    This reverts commit 7ee02f4e5215eae2968f20ef06a19a5767488d89.

---
+++ src/GregClient/NuGet/CreateNugetPackage.ps1
@@ -0,0 +1,23 @@
+﻿param(
+[string]$nuget= $(throw "-nuget is required."),
+[string]$out=$(throw "-out is required")
+)
+
+if(!(Test-Path -Path $nuget\lib\net45\)){
+    New-Item -ItemType directory -Path $nuget\lib\net45\
+}
+
+
+if(!(Test-Path -Path $nuget\content)){
+    New-Item -ItemType directory -Path $nuget\content
+}
+
+if(!(Test-Path -Path $nuget\content\controllers)){
+    New-Item -ItemType directory -Path $nuget\content\controllers
+}
+
+copy $out\Greg.dll $nuget\lib\net45\
+
+$dllVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$out\Greg.dll").FileVersion
+
+nuget pack -version $dllVersion -OutputDirectory $nuget $nuget\GregClient.nuspec 
