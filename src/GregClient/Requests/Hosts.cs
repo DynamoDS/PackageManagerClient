02f822f11137cb99f796c2d79c485551446aca6e
pinzart <tiberiu.pinzariu@autodesk.com>
2019-07-01 09:18:01 -0400

    use spaces

--- src/GregClient/Requests/Hosts.cs
+++ src/GregClient/Requests/Hosts.cs
@@ -6,10 +6,10 @@ using RestSharp;
 
 namespace Greg.Requests
 {
-	/// <summary>
-	/// Hosts request. Returns a fixed list of all supported host applications.
-	/// Ex. ["Revit", "Autocad", ...]
-	/// </summary>
+    /// <summary>
+    /// Hosts request. Returns a fixed list of all supported host applications.
+    /// Ex. ["Revit", "Autocad", ...]
+    /// </summary>
     public class Hosts : Request
     {
         public Hosts()
