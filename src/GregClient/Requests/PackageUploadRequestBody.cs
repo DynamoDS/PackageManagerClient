f9622d13a11e28259cf65273e000d034f34f660b
Michael Kirschner <mjk.kirschner@gmail.com>
2019-08-14 12:52:11 -0400

    add back the old constructors and use them to save some repeat code for now (#31)

--- src/GregClient/Requests/PackageUploadRequestBody.cs
+++ src/GregClient/Requests/PackageUploadRequestBody.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Greg.Requests
 {
@@ -9,7 +10,25 @@ namespace Greg.Requests
             string contents, string engine, string engineVersion,
             string metadata, string group, IEnumerable<PackageDependency> dependencies,
             string siteUrl, string repositoryUrl, bool containsBinaries, 
-            IEnumerable<string> nodeLibraryNames, IEnumerable<string> hostDependencies)
+            IEnumerable<string> nodeLibraryNames, IEnumerable<string> hostDependencies):
+
+            this(name,version,description,
+                keywords,license,
+                contents,engine,engineVersion, 
+                metadata,group,dependencies,
+                siteUrl,repositoryUrl,containsBinaries,
+                nodeLibraryNames)
+        {
+            this.host_dependencies = hostDependencies;
+        }
+
+        [Obsolete("This constructor will be removed in a future release of packageManagerClient.")]
+        public PackageUploadRequestBody(string name, string version, string description,
+           IEnumerable<string> keywords, string license,
+           string contents, string engine, string engineVersion,
+           string metadata, string group, IEnumerable<PackageDependency> dependencies,
+           string siteUrl, string repositoryUrl, bool containsBinaries,
+           IEnumerable<string> nodeLibraryNames)
         {
             this.name = name;
             this.version = version;
@@ -25,7 +44,6 @@ namespace Greg.Requests
             this.repository_url = repositoryUrl;
             this.contains_binaries = containsBinaries;
             this.node_libraries = nodeLibraryNames;
-            this.host_dependencies = hostDependencies;
 
             this.license = license;
         }
