6b8e4c3b970c58dd87a5fd739a3742d1cf9fd173
Martin Misol Monzo <mmisol@users.noreply.github.com>
2020-03-25 17:22:45 -0400

    Expose endpoints to get package version metadata
    This adds a request type HeaderVersionDownload that supports either:
    - package id, package version
    - engine, package name, package version
    
    The response is a PackageVersion, which will contain just the ids for
    dependencies, but preserves the existing structure. This is achieved by
    using custom JSON serialization with DependencyConverter.

---
+++ src/GregClient/Converters/DependencyConverter.cs
@@ -0,0 +1,51 @@
+﻿using Greg.Responses;
+using Newtonsoft.Json;
+using System;
+
+namespace Greg.Converters
+{
+    /// <summary>
+    /// Custom converter that supports deserializing a Dependency from either:
+    /// - a string, which is interpreted as the id of the dependency
+    /// - an object having the expected properties, which is the default behavior
+    /// </summary>
+    public class DependencyConverter : JsonConverter
+    {
+        public override bool CanWrite
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Dependency);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.ValueType == typeof(string))
+            {
+                // This is interpreted as the id of the dependency.
+                Dependency dep = (Dependency)existingValue ?? new Dependency();
+                dep._id = (string)reader.Value;
+                return dep;
+            }
+            else
+            {
+                // Use the default deserialization behavior.
+                existingValue = existingValue ?? serializer.ContractResolver.ResolveContract(objectType).DefaultCreator();
+                serializer.Populate(reader, existingValue);
+                return existingValue;
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            // Not needed as CanWrite is false.
+            throw new NotImplementedException();
+        }
+    }
+}
