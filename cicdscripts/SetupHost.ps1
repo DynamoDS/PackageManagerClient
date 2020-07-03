b7caecf2378f2ee1a7da60ef1cf414621645c852
Alfredo Pozo <alfredo.pozo@globant.com>
2020-07-02 20:55:10 -0500

    Adding python for WS scan

--- cicdscripts/SetupHost.ps1
+++ cicdscripts/SetupHost.ps1
@@ -5,7 +5,9 @@ $ErrorActionPreference = "Stop"
 
 #Getting directory names to install the tools
 $nugetSubDir = ($env:NUGETTOOL -split "\\")[0]
+$elementsPath = ($env:WORKSPACE -split "\\")
 
+$toolsDir = $elementsPath[0] + '\' + $elementsPath[1] + '\tools'
 $nugetDir = "$env:WORKSPACE\$env:COMMON_TOOLS_DIR\$nugetSubDir"
 
 #Creation of the tools directories
@@ -13,3 +15,16 @@ New-Item -Path $nugetDir  -ItemType Directory
 
 #Download of latest version of Nuget
 Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "$nugetDir\nuget.exe"
+
+try {
+   #Instal Python from nuget
+   &"$env:WORKSPACE\$env:COMMON_TOOLS_DIR\$env:NUGETTOOL" install python -source nuget.org -ExcludeVersion -OutputDirectory $toolsDir
+
+   if($LASTEXITCODE -ne 0) {
+      throw "Install of Python has failed"
+   }
+}
+catch {
+   Write-Host $error[0]
+   throw $LASTEXITCODE   
+}
