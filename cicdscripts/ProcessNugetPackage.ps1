cb7d5c39c64205a89e24674dd99f7c8bb16a3a34
alfredo-pozo <47230272+alfredo-pozo@users.noreply.github.com>
2020-07-09 18:33:32 -0500

    Changes to update to Net48 and CI/CD pipeline addition (#39)
    * First version of Greg with Net48 and the CI/CD pipeline
    
    * Update BuildSolution.ps1
    
    * Update BuildSolution.ps1
    
    * Change to remove postbuild event and adding PostBuild script
    
    * Update nuspec file
    
    * Remove Nuget commandline package because now the package is created on the execution of the CI/CD pipeline
    
    * Adding python for WS scan
    
    * Changes to the deploy script
    
    * Change to remove list of emails and switch to a list inside a Jenkins credential
    
    * Changes to README.md
    
    * Changes requested to README
    
    * Changes to excute the deploy from the Docker Container
    
    * Fix nuget path

---
+++ cicdscripts/ProcessNugetPackage.ps1
@@ -0,0 +1,36 @@
+﻿<#
+Date: 07/02/2020
+Purpose: To create and publish the Greg nuget package
+#>
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [string]
+    $Workspace,
+    [Parameter(Mandatory)]
+    [string]
+	$NugetPath,
+	[Parameter(Mandatory)]
+    [string]
+    $ApiKey
+)
+
+$ErrorActionPreference = "Stop"
+
+$assemblyPath = "$Workspace\bin\Debug"
+$nuspecPath = "$Workspace\nuspec"
+
+$dllVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$assemblyPath\Greg.dll").FileVersion
+
+try {
+
+	& "$NugetPath" pack -basePath $assemblyPath -version $dllVersion -OutputDirectory $nuspecPath $nuspecPath\GregClient.nuspec
+
+	$nupkgFile = Get-ChildItem $nuspecPath\*.nupkg -Depth 1
+
+	& "$NugetPath" push $nupkgFile.FullName -ApiKey $ApiKey -Source nuget.org
+}
+catch {	
+	Write-Host $error[0]
+	throw $LASTEXITCODE
+}
