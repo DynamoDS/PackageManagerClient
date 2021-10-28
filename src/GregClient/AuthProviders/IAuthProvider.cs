4bbe4974fd155238aabed86d23b3b0ed7db7a28f
michael kirschner <michael.kirschner@autodesk.com>
2021-10-28 12:34:37 -0400

    docs, new interface, new path for oauth2

--- src/GregClient/AuthProviders/IAuthProvider.cs
+++ src/GregClient/AuthProviders/IAuthProvider.cs
@@ -11,10 +11,29 @@ namespace Greg
 
         LoginState LoginState { get; }
         string Username { get; }
-
+        /// <summary>
+        /// This method should sign the request and params as OAuth1.
+        /// OAuth parameters should be added to the query string.
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="client"></param>
         void SignRequest(ref RestRequest m, RestClient client);
 
         void Logout();
         bool Login();
+    }
+
+    /// <summary>
+    /// Implement this interface to use OAuth2 for authentication with Dynamo Package Manager.
+    /// </summary>
+    public interface IOAuth2AuthProvider : IAuthProvider
+    {
+        /// <summary>
+        /// This method should add the JWT access token to the Authorization header.
+        /// Package manager expects a header in the form: Authorization: Bearer [accesstoken]
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="client"></param>
+        new void SignRequest(ref RestRequest m, RestClient client);
     }
 }
