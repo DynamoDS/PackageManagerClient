4cdf6831fd2d56db6f0fd7bccb630575fc735ff2
pinzart90 <46732933+pinzart90@users.noreply.github.com>
2022-04-15 12:57:23 -0400

    refactor versioning (#64)
    * update
    
    * Update Utilities.cs
    
    * Update AssemblyInfo.tt
    
    * Update GregClient.csproj
    
    * update version
    
    Co-authored-by: pinzart <tiberiu.pinzariu@autodesk.com>

--- src/GregClient/Properties/AssemblyInfo.cs
+++ src/GregClient/Properties/AssemblyInfo.cs
@@ -2,24 +2,4 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Greg")]
-[assembly: AssemblyDescription("The Dynamo Package Manager .NET client.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Autodesk, Inc.")]
-[assembly: AssemblyProduct("Greg")]
-[assembly: AssemblyCopyright("Copyright © Autodesk, Inc. 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b5ebb49a-bfb3-4aff-8411-2f42b0e6854e")]
-
 [assembly: InternalsVisibleTo("GregClientSandbox")]
