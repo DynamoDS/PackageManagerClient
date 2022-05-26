8391d795a177c890acced72cfda712f143e4ff12
Ashish Aggarwal <ashish.aggarwal@autodesk.com>
2022-05-26 17:18:04 -0400

    add route to fetch user packages

---
+++ src/GregClient/Requests/GetUserPackages.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+
+namespace Greg.Requests
+{
+    public class GetUserPackages : PackageReferenceRequest
+    {
+        private readonly Method httpMethod = Method.GET;
+
+        public GetUserPackages()
+        {
+            this.ForceAuthentication = true;
+
+            httpMethod = Method.GET;
+        }
+
+        public override string Path
+        {
+            get { return "user/latest_packages"; }
+        }
+
+        public override Method HttpMethod
+        {
+            get { return httpMethod; }
+        }
+
+        internal override void Build(ref RestRequest request)
+        {
+        }
+    }
+}
