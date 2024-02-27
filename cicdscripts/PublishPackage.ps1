4089f3791a2aa917362e65a60a0f40d6712e68ce
kirschm <michael.kirschner@autodesk.com>
2024-02-27 16:20:36 -0500

    revert debug changes

--- cicdscripts/PublishPackage.ps1
+++ cicdscripts/PublishPackage.ps1
@@ -25,13 +25,11 @@ $reBranch = [regex]$regex
 if ($env:BRANCH_NAME -match $reBranch) 
 {
 	try {
-		Set-PSDebug -Trace 2;
 		#deploy already built package.
 		$assemblyPath = "$env:WORKSPACE\bin\release"
 		$nupkgFile = Get-ChildItem $assemblyPath\*.nupkg -Depth 1
 		Write-Host $env:NUGET_PUBLISH_SOURCE
 		Write-Host $nupkgFile
-		Write-Host $env:APIKEY
 		dotnet nuget push $nupkgFile --api-key $env:APIKEY --source $env:NUGET_PUBLISH_SOURCE
 
 		if($LASTEXITCODE -ne 0) {
@@ -39,7 +37,6 @@ if ($env:BRANCH_NAME -match $reBranch)
 		}
 	}
 	catch {
-		Set-PSDebug -Trace 0;
 		Write-Host $error[0]
 		throw $LASTEXITCODE
 	}
